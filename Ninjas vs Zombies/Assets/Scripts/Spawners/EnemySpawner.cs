16b95e25a91492f4388fe6650a7f21284f7131d7
Amador Cuenca <sphi02ac@gmail.com>
2019-08-21 07:57:00 -0500

    Create the base enemy spawner

---
+++ Ninjas vs Zombies/Assets/Scripts/Spawners/EnemySpawner.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+
+namespace NinjasVsZombies.Spawners
+{
+    public class EnemySpawner : MonoBehaviour
+    {
+        [Header("Prefab")]
+        [SerializeField] private GameObject[] _enemyPrefabs;
+
+        [Header("Spawn Frequency")]
+        [SerializeField] private float _minSpawnDelay;
+        [SerializeField] private float _maxSpawnDelay;
+
+        private float _spawnDelay;
+        private float _nextThrowTime;
+    }
+}
