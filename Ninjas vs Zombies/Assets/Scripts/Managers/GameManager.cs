c1cb79674021dcfb73562673d5fc7be255692ebe
Amador Cuenca <sphi02ac@gmail.com>
2019-09-04 13:59:37 -0500

    Create the GameManager singleton

---
+++ Ninjas vs Zombies/Assets/Scripts/Managers/GameManager.cs
@@ -0,0 +1,31 @@
+﻿using NinjasVsZombies.Utils;
+using UnityEngine;
+
+namespace NinjasVsZombies.Managers
+{
+    public class GameManager : MonoBehaviour
+    {
+        public static GameManager instance;
+
+        [HideInInspector]
+        public GameStatus gameStatus;
+
+        private void Awake()
+        {
+            gameStatus = GameStatus.NOT_STARTED;
+        }
+
+        private void MakeSingleton()
+        {
+            if (instance != null)
+            {
+                Destroy(gameObject);
+            }
+            else
+            {
+                instance = this;
+                DontDestroyOnLoad(gameObject);
+            }
+        }
+    }
+}
---
+++ Ninjas vs Zombies/Assets/Scripts/Utils/GameStatus.cs
@@ -0,0 +1,10 @@
+﻿namespace NinjasVsZombies.Utils
+{
+    public enum GameStatus
+    {
+        NOT_STARTED,
+        PLAYING,
+        PAUSED,
+        GAME_OVER
+    }
+}
