2edb327b54e15994ec4dfdb6b3d844f0e6033532
Amador Cuenca <sphi02ac@gmail.com>
2019-09-05 13:19:53 -0500

    Complete the Highscore screen

---
+++ Ninjas vs Zombies/Assets/Scripts/UI/HighscoreUI.cs
@@ -0,0 +1,23 @@
+﻿using NinjasVsZombies.Utils;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+namespace NinjasVsZombies.UI
+{
+    public class HighscoreUI : MonoBehaviour
+    {
+        [SerializeField] private Text _highscoreText;
+
+        private void Update()
+        {
+            int highscore = HighscoreDB.Instance.GetHighScore();
+            _highscoreText.text = highscore.ToString();
+        }
+
+        public void BackToMainMenu()
+        {
+            SceneManager.LoadScene("MainMenu");
+        }
+    }
+}
