2fac33a20bb3403fe9dfee79cd9919e1752568bd
Amador Cuenca <sphi02ac@gmail.com>
2019-09-05 17:10:25 -0500

    Create the SoundManager
    Created the SoundManager and import the SFXs.

---
+++ Ninjas vs Zombies/Assets/Scripts/Managers/AudioManager.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+
+namespace NinjasVsZombies.Managers
+{
+    public class AudioManager : MonoBehaviour
+    {
+        public static AudioManager instance;
+
+        [SerializeField] private AudioSource _audioSource;
+
+        private void Awake()
+        {
+            MakeSingleton();
+        }
+
+        private void MakeSingleton()
+        {
+            if (instance != null)
+            {
+                Destroy(gameObject);
+            }
+            else
+            {
+                instance = this;
+                DontDestroyOnLoad(gameObject);
+            }
+        }
+
+        public void PlayClip(AudioClip audioClip)
+        {
+            _audioSource.clip = audioClip;
+            _audioSource.Play();
+        }
+    }
+}
